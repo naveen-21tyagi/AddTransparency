9336f202b92a7b4300984f5a74d619742c07bfba
naveen-21tyagi <naveentyagi68@gmail.com>
2023-07-16 17:34:24 +0530

    Initial version

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace AddTransparency;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new App());
+    }    
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# AddTransparency
+
+This application can make your programs transparent.
+- Alpha is 221 on scale of (0-255)
+
+### Requirements
+> dotnet 6.0
+
